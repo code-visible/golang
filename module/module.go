deccd8d999d5563f2fe8ec3c60dde9afbb36b8bf
ChallenAi <fengyiai@gmail.com>
2024-02-10 11:07:58 +0000

    doc: module initialization

--- module/module.go
+++ module/module.go
@@ -2,13 +2,50 @@ package module
 
 import (
 	"go/ast"
+	"go/parser"
 	"go/token"
+
+	"github.com/code-visible/golang/utils"
 )
 
 type Module struct {
 	Name  string   `json:"name"`
 	Path  string   `json:"path"`
 	Files []string `json:"files"`
-	fs    *token.FileSet
-	pkgs  map[string]*ast.Package
+
+	fs   *token.FileSet
+	pkgs map[string]*ast.Package
+}
+
+func NewModule(path string) (*Module, error) {
+	// make sure the given path is a directory
+	err := utils.MustDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// initialize module
+	m := &Module{
+		Name:  "",
+		Path:  path,
+		Files: nil,
+		fs:    token.NewFileSet(),
+		pkgs:  nil,
+	}
+
+	// TODO: decide whether the comment should be parsed
+	pkgs, err := parser.ParseDir(m.fs, path, nil, parser.ParseComments)
+	if err != nil {
+		return nil, err
+	}
+
+	m.pkgs = pkgs
+
+	// exported files
+	m.fs.Iterate(func(f *token.File) bool {
+		m.Files = append(m.Files, f.Name())
+		return true
+	})
+
+	return m, nil
 }

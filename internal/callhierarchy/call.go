96d30b2e9f8c1a9dae1086f7aa5ad447151d6cb7
ChallenAi <fengyiai@gmail.com>
2024-06-28 06:51:31 +0000

    feat: create Call struct to represent call operation, with caller and callee

---
+++ internal/callhierarchy/call.go
@@ -0,0 +1,43 @@
+package callhierarchy
+
+import (
+	"fmt"
+	"go/token"
+
+	"github.com/code-visible/golang/internal/parsedtypes"
+)
+
+type Call struct {
+	ID        string `json:"id"`
+	Caller    int    `json:"caller"`
+	Callee    int    `json:"callee"`
+	File      int    `json:"file"`
+	Typ       string `json:"typ"`
+	Signature string `json:"signature"`
+
+	pos      token.Pos
+	scope    string
+	selector string
+	typ      *parsedtypes.Type
+}
+
+func NewCall(pos token.Pos, scope string, selector string, typ *parsedtypes.Type) *Call {
+	return &Call{
+		pos:      pos,
+		scope:    scope,
+		selector: selector,
+		typ:      typ,
+	}
+}
+
+func (c *Call) Complete() {
+	if c.typ != nil {
+		c.Signature = fmt.Sprintf("(%s).%s()", c.typ, c.selector)
+		return
+	}
+	if c.scope != "" {
+		c.Signature = fmt.Sprintf("%s.%s()", c.scope, c.selector)
+		return
+	}
+	c.Signature = fmt.Sprintf("%s()", c.selector)
+}

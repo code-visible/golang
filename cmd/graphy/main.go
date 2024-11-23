24a0f8c607205acae96523f0fef8afd8a5d7794e
ChallenAi <fengyiai@gmail.com>
2024-11-23 11:20:23 +0000

    feat: provide entry arguments for executable binary

--- cmd/graphy/main.go
+++ cmd/graphy/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/code-visible/golang/parser"
 )
 
 func main() {
-	const project = "/root/go/src/github.com/challenai/golang"
-	const direcotry = "testdata/hierarchy"
-	p := parser.NewProject(project, direcotry)
+	var project string
+	var directory string
+	flag.StringVar(&project, "project", ".", "path of the project")
+	flag.StringVar(&directory, "directory", ".", "directory of the project to parse")
+	flag.Parse()
+	fmt.Printf("graphy: try to parse project (%s) with folder (%s)\n", project, directory)
+	p := parser.NewProject(project, directory)
 	p.Initialize()
 	p.Parse()
-	fmt.Println(p)
+	d, err := json.Marshal(p)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile("compiled.json", d, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
